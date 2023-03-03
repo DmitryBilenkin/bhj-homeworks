dde40479dc4a7d2a2c621e05abdddcf5a07f9c4d
Dmitriy Bilenkin <spayk94@gmail.com>
2023-03-03 23:27:49 +0300

    Version 2

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,11 +2,12 @@ let rectsOnDocument = Array.from(document.querySelectorAll('.reveal'))
 
 rectsOnDocument.forEach((rect) => {
     setInterval(()=>{
-        const {top, bottom} = rect.getBoundingClientRect();
-        if(bottom < 0 || top > window.innerHeight){
-            rect.classList.remove('reveal_active');
-        } else {
-            rect.classList.add('reveal_active');
-        };
-    },1000)    
+        const {innerHeight} = window;
+        const {top} = rect.getBoundingClientRect();        
+        if (top < innerHeight && top > 0) {        
+          rect.classList.add("reveal_active");        
+        } else {        
+          rect.classList.remove("reveal_active");        
+        }        
+    }, 1000)
 });
