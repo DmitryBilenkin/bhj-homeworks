a8606bb3833447c4103cc231cc2b48fa751f177b
Dmitriy Bilenkin <spayk94@gmail.com>
2023-03-06 20:09:34 +0300

    Red 3

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,13 +1,13 @@
 let rectsOnDocument = Array.from(document.querySelectorAll('.reveal'))
 
 rectsOnDocument.forEach((rect) => {
-    setInterval(()=>{
-        const {innerHeight} = window;
-        const {top} = rect.getBoundingClientRect();        
-        if (top < innerHeight && top > 0) {        
+    document.addEventListener('scroll', () => {
+      const {innerHeight} = window;
+        const {top, bottom} = rect.getBoundingClientRect();        
+        if (top < innerHeight && bottom > 0) {        
           rect.classList.add("reveal_active");        
         } else {        
           rect.classList.remove("reveal_active");        
-        }        
-    }, 1000)
+        }
+    })
 });
